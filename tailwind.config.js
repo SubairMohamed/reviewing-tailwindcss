e49e81cb248ffe003df2b921552cb144f3274dab
SubairMohamed <SubairMohamed44@gmail.com>
2024-06-07 22:26:28 +0300

    well

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        headline: ["Tiny5"],
+      },
+    },
   },
   plugins: [],
 };
